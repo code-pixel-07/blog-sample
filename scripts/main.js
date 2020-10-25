84e05ceb561d0ab50c73ba41241a7ba76b185cae
code-pixel-07 <sharonshaji127001@gmail.com>
2020-10-25 15:58:24 +0530

    Sidebar created (popular section) finished. Moving on to create newsletter Section and a quicklink tags section

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
     // owl carousel
     $('.owl-carousel').owlCarousel({
         loop:true, 
-        autoplay:false,
-        autoplayTimeout: 3000,
+        autoplay:true,
+        autoplayTimeout: 2000,
         dots:false, 
         nav:true,
         navText : [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
