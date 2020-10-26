9f466499a5f76e4b38f6648348dd088b5829504b
code-pixel-07 <sharonshaji127001@gmail.com>
2020-10-26 19:58:07 +0530

    Site is now responsive. Moving on to implement animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,22 @@
 
+const responsive = {
+    0: {
+        items:1
+    },
+    320: {
+        items:1
+    },
+    520: {
+        items:2
+    },
+    960: {
+        items:3
+    },
+    1200: {
+        items:4
+    }
+    
+}
 
 $(document).ready(function () {
     // owl carousel
@@ -9,7 +27,7 @@ $(document).ready(function () {
         dots:false, 
         nav:true,
         navText : [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        responsive:{}
+        responsive: responsive
     }) ;
 
 
